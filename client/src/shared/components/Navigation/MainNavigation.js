87963a0eb10384ec792418b6536a5e139867181c
shahryar <shahryarsaeed06@gmail.com>
2024-01-15 23:20:41 +0500

    Adding Styles to MainNavigation

--- client/src/shared/components/Navigation/MainNavigation.js
+++ client/src/shared/components/Navigation/MainNavigation.js
@@ -1,24 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './MainNavigation.css';
 // Components
 import NavLinks from './NavLinks';
 import MainHeader from './MainHeader';
+import SideDrawer from './SideDrawer';
 
 const MainNavigation = () => {
 
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+
+
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+
   return (
     <>
-       
-       <MainHeader>
-       <h1 className='main-navigation__title'>
-        <Link to='/'>Your Places </Link>
-       </h1>
+      {drawerIsOpen && 'Hello Drawer'}
 
-       <nav className='main-navigation__header-nav'>
-        <NavLinks/>
-       </nav>
-       </MainHeader>
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className='main-navigation__drawer-nav'>
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+
+      <MainHeader>
+
+      <button className='main-navigation__menu-btn' onClick={openDrawerHandler}>
+          <span />
+          <span />
+          <span />
+        </button>
+
+
+        <h1 className='main-navigation__title'>
+          <Link to='/'>Your Places </Link>
+        </h1>
+
+        <nav className='main-navigation__header-nav'>
+          <NavLinks />
+        </nav>
+      </MainHeader>
     </>
   )
 }
